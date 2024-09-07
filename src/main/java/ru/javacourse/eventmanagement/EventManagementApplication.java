a7f7495dffe4590773308d7b2cdf3396b18635cd
Alexandr Pyaternev <pjatera08@gmail.com>
2024-09-07 20:53:46 +0300

    first commit

---
+++ src/main/java/ru/javacourse/eventmanagement/EventManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.javacourse.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javacourse/eventmanagement/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javacourse.eventmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
